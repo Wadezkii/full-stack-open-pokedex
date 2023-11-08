a26db1e686a199f66f6353b1d7575496088ce7fd
Wadezkii <weeti.tiirikainen@hotmail.fi>
2023-11-08 19:41:13 +0200

    im losing my mind

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './App';
-import './styles.css';
+import React from 'react'
+import { createRoot } from 'react-dom'
+import App from './App'
+import './styles.css'
 
-const root = document.getElementById('app');
-const reactRoot = createRoot(root);
-reactRoot.render(<App />);
+const root = document.getElementById('app')
+const reactRoot = createRoot(root)
+reactRoot.render(<App />)
